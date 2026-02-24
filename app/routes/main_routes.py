052624a7f954f238db706471d81646ab883d2703
Phuluso <119367220+PS-ThePhantom@users.noreply.github.com>
2026-02-24 21:43:15 +0200

    feat: add boiler templates and static files

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -1,6 +1,10 @@
-from flask import render_template, jsonify
+from flask import render_template
 from . import main_bp
 
-@main_bp.route("/")
+@main_bp.route("/", methods=["GET"])
 def home():
-    return "<h1>Welcome to your Flask App!</h1>"
+    return "<h1>Welcome to your Flask App!</h1>", 200
+
+@main_bp.route("/<path:invalid_path>")
+def page_not_found(invalid_path):
+    return "<h1 style='color: red; text-align: center;'>Page Not Found</h1>", 404
